61116aad3c3116b0755af08e138059a223442cf6
Akos <benczikakos@gmail.com>
2023-11-03 18:15:20 +0100

    avg_composite sort of

--- src/shaders/fshad_avg.glsl
+++ src/shaders/fshad_avg.glsl
@@ -1,89 +1,30 @@
 #version 150
 out highp vec4 fragColor;
 
-uniform mat4 ViewMatrix;
 uniform vec2 WindowSize;
-uniform vec3 CameraPos;
-uniform vec3 AABBScale;
+uniform float stepLength; 
+uniform sampler2D enterTexture;
+uniform sampler2D exitTexture;
 uniform sampler3D Volume;
 uniform sampler2D TransferFunction;
-uniform float intensityMin;
-uniform float intensityMax;
-uniform float stepLength;
-
-
-struct Ray {
-    vec3 Origin;
-    vec3 Dir;
-};
-
-struct AABB {
-    vec3 Min;
-    vec3 Max;
-};
-
-bool IntersectBox(Ray r, AABB aabb, out float t0, out float t1)
-{
-    vec3 invR = 1.0 / r.Dir;
-    vec3 tbot = invR * (aabb.Min-r.Origin);
-    vec3 ttop = invR * (aabb.Max-r.Origin);
-    vec3 t_enter = min(ttop, tbot);
-    vec3 t_exit = max(ttop, tbot);
-    t0 = max(t_enter.x, max(t_enter.y, t_enter.z));
-    t0 = max(0, t0);
-    t1 = min(t_exit.x, min(t_exit.y, t_exit.z));
-    return t1 > t0;
-}
-
-float cap(float value, float min, float max) {
-    if (value > max || value < min) {
-        return 0.0;
-    } else {
-        return value;
-    }
-}
-
-
-
-vec4 color_transfer(float intensity)
-{
-    vec3 high = vec3(1.0, 1.0, 1.0);
-    vec3 low = vec3(0.0, 0.0, 0.0);
-    float alpha = (exp(intensity) - 1.0) / (exp(1.0) - 1.0);
-    return vec4(intensity * high + (1.0 - intensity) * low, alpha);
-}
 
 void main()
-{
-   vec3 rayDirection;
-   rayDirection.xy = 2.0f * gl_FragCoord.xy / WindowSize - 1.0;
-   rayDirection.z = 2.0f;
-   rayDirection = (vec4(rayDirection, 0) * ViewMatrix).xyz;
-
-   Ray eye = Ray(CameraPos, normalize(rayDirection));
-   AABB aabb = AABB(vec3(-1.0f)*AABBScale, vec3(1.0f)*AABBScale);
-
-   float tnear, tfar;
-    if(IntersectBox(eye, aabb, tnear, tfar)){
-        vec3 rayStart = (eye.Origin + eye.Dir * tnear - aabb.Min) / (aabb.Max - aabb.Min);
-        vec3 rayStop = (eye.Origin + eye.Dir * tfar - aabb.Min) / (aabb.Max - aabb.Min);
-        // Perform the ray marching:
-        vec3 pos = rayStart;
-        vec3 step = normalize(rayStop-rayStart) * stepLength;
-        float travel = distance(rayStart, rayStop);
-        float intensity_sum = 0.0f;
-        vec4 color = vec4(0.0f);
-        int i=0;
-        for (; i < 1.0/stepLength && travel > 0.0; ++i, pos += step, travel -= stepLength)
-        {
-            float intensity = texture(Volume, pos).r;
-            intensity = cap(intensity, intensityMin, intensityMax);
-            intensity_sum += intensity;
-        }
-        float avg_intensity = intensity_sum / i * 5.0f;
-        fragColor = texture(TransferFunction, vec2(avg_intensity, 0.5f));
+{   
+    vec2 screenPos = gl_FragCoord.xy / WindowSize;
+    vec3 enterPoint = texture(enterTexture, screenPos).xyz;
+    vec3 exitPoint = texture(exitTexture, screenPos).xyz;
+    vec3 travel = exitPoint - enterPoint;
+    float travelLength = length(travel);
+    vec3 pos = enterPoint;
+    float sumIntensity = 0.0f;
+    int i = 0;
+    for(float t = 0.0f; t < travelLength; t += stepLength, ++i)
+    {
+        pos = enterPoint + t * normalize(travel);
+        sumIntensity += texture(Volume, pos).r;
+        
     }
-   else{
-       fragColor = vec4(0.0f);
-   }
+    float avgInstensity = sumIntensity / (travelLength/stepLength);
+    fragColor = texture(TransferFunction, vec2(avgInstensity, 0.5f));
+    return;
 }
