2d4990aba5d2dcb6bcaa482410a39e2589bae77f
Akos <benczikakos@gmail.com>
2023-11-03 17:47:05 +0100

    mip seems cool

--- src/shaders/fshad_mip.glsl
+++ src/shaders/fshad_mip.glsl
@@ -1,15 +1,31 @@
 #version 150
 out highp vec4 fragColor;
 
-uniform vec2 WindowSize;    
+uniform vec2 WindowSize;
+uniform float stepLength; 
 uniform sampler2D enterTexture;
 uniform sampler2D exitTexture;
+uniform sampler3D Volume;
 
 void main()
-{
+{   
     vec2 screenPos = gl_FragCoord.xy / WindowSize;
     vec3 enterPoint = texture(enterTexture, screenPos).xyz;
     vec3 exitPoint = texture(exitTexture, screenPos).xyz;
-    fragColor = vec4(enterPoint, 1.0f);
+    vec3 travel = exitPoint - enterPoint;
+    float travelLength = length(travel);
+    vec3 pos = enterPoint;
+    float maxIntensity = 0.0f;
+    for(float t = 0.0f; t < travelLength; t += stepLength)
+    {
+        pos = enterPoint + t * normalize(travel);
+        float intensity = texture(Volume, pos).r;
+        if(intensity > maxIntensity)
+        {
+            maxIntensity = intensity;
+        }
+    }
+    float small = texture(Volume, enterPoint).r;
+    fragColor = vec4(maxIntensity, maxIntensity, maxIntensity, 1.0f);
     return;
 }
